13ba9eef246a710983f978b920f95ba2031ee4b9
Vinícius Rocha <158187426+Vinirocha388@users.noreply.github.com>
2025-03-11 09:28:15 -0300

    feat: create Sidebar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import styles from "./page.module.css";
+import Sidebar from "@/components/Sidebar";
 
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Sidebar />
     </div>
   );
 }
