4123fbc5951958054344159cf26a662b264fa473
Vinícius Rocha <158187426+Vinirocha388@users.noreply.github.com>
2025-03-11 11:09:11 -0300

    feat: create dashboard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,18 @@
 import Header from "@/components/Header";
 import styles from "./page.module.css";
 import Sidebar from "@/components/Sidebar";
+import DashboardContent from "@/components/DashboardContent";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.app}>
       <Header />
+      <main className={styles.dashboardContainer}>
       <Sidebar />
+      <DashboardContent />
+      </main>
+      
     </div>
   );
 }
